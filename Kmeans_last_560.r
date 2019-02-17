e6fc2a1485670b626dcda7efe494311878e32d0f
Dev Shrestha <dev.shrestha025@gmail.com>
2019-02-16 21:52:16 -0800

    Update Kmeans_last_560.r

--- Kmeans_last_560.r
+++ Kmeans_last_560.r
@@ -18,15 +18,18 @@ length(which(is.na(cereals)))
 #remove null data from the dataset
 cereals <- cereals[complete.cases(cereals),]
 ```
+
 ```{r}
 # check the names of the data set
 names(cereals)
 ```
+
 ```{r}
 set.seed(2222)
 #pick which columns to analyze
 cereals_k <- cereals[, -c(1:3,16)]
 ```
+
 ```{r}
 cereals_k[,c(3:14)] <- scale(cereals_k[,c(3:14)])
 dist_cereals_k <- dist(cereals_k)
@@ -36,6 +39,7 @@ library(factoextra)
 #get_dist(cereals_k)
 fviz_cluster(km1, data = cereals_k)
 ```
+
 ```{r}
 #use different libraries for data visualization
 library(cluster)
@@ -45,7 +49,6 @@ dist_cereals_model1<-fviz_silhouette(silhouette(km1$cluster, dist_cereals_k))
 dist_cereals_model1
 ```
 
-
 ```{r}
 #run kmeans cluster algorithm and give 3 centers. 
 km2 <- kmeans(cereals_k,3)
@@ -54,6 +57,7 @@ library(factoextra)
 #get_dist(cereals_k)
 fviz_cluster(km2, data = cereals_k)
 ```
+
 ```{r}
 #produce a silhouette plot for more analysis
 dist_cereals_model2 <-fviz_silhouette(silhouette(km2$cluster, dist_cereals_k))
